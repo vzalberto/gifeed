0d6e2f60a070277387e2fcca116bc74089ef3587
Alberto <vzalberto@msn.com>
2019-06-30 14:07:23 -0500

    Change placeholder

--- js/reactivity.js
+++ js/reactivity.js
@@ -1,4 +1,5 @@
 const output = document.getElementById("response-list");
+let searchInput = document.getElementById("search-input");
 function showResults(resp) {
         output.innerHTML = "";
         console.log(resp)
@@ -27,24 +28,23 @@ function showResults(resp) {
             });
         }
 }
+const TRENDING_URL = "https://api.giphy.com/v1/gifs/trending?api_key=KQzPKVUFZUIpii6iYFGNphMc7ujV6UcR&limit=12";
 
-    const TRENDING_URL = "https://api.giphy.com/v1/gifs/trending?api_key=KQzPKVUFZUIpii6iYFGNphMc7ujV6UcR&limit=12";
-
+// Se hace una carga inicial 
 Rx.Observable.ajax(TRENDING_URL)
-            .map(resp => ({
-                    "status": resp["status"] == 200,
-                    "details": resp["status"] == 200 ? resp["response"] : [],
-                    "result_hash": Date.now()
-                })
-            )
+	.map(resp => ({
+	        "status": resp["status"] == 200,
+	        "details": resp["status"] == 200 ? resp["response"] : [],
+	        "result_hash": Date.now()
+	    })
+	)
 
-	        .catch(err => Rx.Observable.of({err})
-	        )
-        	.filter(resp => resp.status !== false)
-			.subscribe(resp => showResults(resp.details));
+	.catch(err => Rx.Observable.of({err})
+	)
+	.filter(resp => resp.status !== false)
+	.subscribe(resp => showResults(resp.details));
 
-    let searchInput = document.getElementById("search-input");
-    Rx.Observable.fromEvent(searchInput, 'input')
+Rx.Observable.fromEvent(searchInput, 'input')
         .pluck('target', 'value')
         .filter(searchTerm => searchTerm.length > 2 || searchTerm === "")
         .debounceTime(500)
